e5e0f0cdf9af2771c4a5c4d67d4b4e1d393c333f
Runnlin <z77072716565@gmail.com>
2020-05-18 16:50:48 +0800

    UART

---
+++ STM32L431RC_BearPiUART/STM32L431RC_BearPiUART/MDK-ARM/RTE/_STM32L431RC_BearPiUART/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32L431RC_BearPiUART' 
+ * Target:  'STM32L431RC_BearPiUART' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
